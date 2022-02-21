6f421d408fd6cd51a73ffc5b02f2d637bc6041eb
Kyle <61264165+KylJin@users.noreply.github.com>
2022-02-21 15:42:24 +0800

    Delete env directory

--- env/objects/__init__.py
+++
@@ -1,7 +0,0 @@
-from .oilfield import OilField
-from .purchase import Purchase
-from .transfer import Transfer
-from .refinery import Refinery
-from .saleregion import SaleRegion
-from .province import Province
-from .road import Road
--- env/objects/road.py
+++
@@ -1,8 +0,0 @@
-class Road(object):
-    def __init__(self, config):
-        self.way = config['way']
-        self.goods = config['goods']
-        self.lower_capacity = config['lower_capacity']
-        self.upper_capacity = config['upper_capacity']
-        self.cost = config['cost']
-        self.time = config['time']
